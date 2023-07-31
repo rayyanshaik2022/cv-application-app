14577a2b68944f6e24f28faad57350eb34f6d8cf
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-07-31 17:24:57 -0400

    Skill add/delete + refactoring so that jsx is not in state

--- src/SkillsItem.jsx
+++ src/SkillsItem.jsx
@@ -1,11 +1,26 @@
 import { Flex, Input, Button } from "@chakra-ui/react";
+import { useState } from "react";
 
 function SkillsItem(props) {
-  return (
+  
+    const [inputValue, setInputValue] = useState(props.skill != null ? props.skill : "");
+
+    let inputHandler = (e) => {
+        let val = e.target.value;
+        setInputValue(val);
+    }
+  
+    return (
     <Flex flexDir={"column"} gap={"20px"} mb={"20px"}>
       <Flex gap={"12px"}>
-        <Input placeholder=" " variant="outline" bg={"gray.50"} value={props.skill}/>
-        <Button colorScheme="red" onClick={console.log(props.handleRemove)}>Remove</Button>
+        <Input
+          placeholder=" "
+          variant="outline"
+          bg={"gray.50"}
+          value={inputValue}
+          onChange={inputHandler}
+        />
+        <Button colorScheme="red" onClick={() => {props.removeHandle(props.id)}}>Remove</Button>
       </Flex>
     </Flex>
   );
